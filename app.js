8c8c1fce23300cc9c1914c885f55dd72a9d0c30d
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-10-05 15:45:16 -0700

    Given an array of location names, will retrieve GPS coordinates and write the location name and coordinates to a local file

--- app.js
+++ app.js
@@ -2,28 +2,43 @@ const { config } = require('dotenv');
 const { Client } = require('@googlemaps/google-maps-services-js');
 const client = new Client({});
 const fs = require('fs');
-const path = require('path');
 
 config();
 
-client
-  .findPlaceFromText({
-    params: {
-      input: 'Anne Arundel County Public Library',
-      inputtype: 'textquery',
-      fields: ['name', 'geometry'],
-      key: process.env.GOOGLE_API_KEY
-    }
-  })
-  .then(({ data }) => {
-    const filePath = path.join(__dirname, 'results.json');
-    const fileContent = JSON.stringify(data.candidates);
-    fs.writeFile(filePath, fileContent, (err) => {
-      if (err) {
-        console.log(err);
-      }
+const sampleInfo = [
+  'ALACHUA CNTY LIBR DIST',
+  'ALAMANCE CNTY PUB LIBRS',
+  'AMARILLO PUB LIBR',
+  'ANACORTES PUB LIBR',
+  'ANCHORAGE PUB LIBR'
+];
+
+const writeStream = fs.createWriteStream('locations.json', {
+  flags: 'a'
+});
+
+sampleInfo.forEach((library) => {
+  client
+    .findPlaceFromText({
+      params: {
+        input: library,
+        inputtype: 'textquery',
+        fields: ['name', 'geometry'],
+        key: process.env.GOOGLE_API_KEY
+      },
+      timeout: 1000
+    })
+    .then(({ data }) => {
+      const locationData = {
+        name: data.candidates[0].name,
+        lat: data.candidates[0].geometry.location.lat,
+        lng: data.candidates[0].geometry.location.lng
+      };
+      const fileContent = JSON.stringify(locationData);
+      writeStream.write(fileContent);
+      console.log(fileContent);
+    })
+    .catch((e) => {
+      console.log(e);
     });
-  })
-  .catch((e) => {
-    console.log(e.response.data);
-  });
+});
