37e5a4b857769b3da4ea6f203e46175a02170494
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-10-09 17:09:50 -0700

    Extracts relevant data from CSV and passes it to Google Maps API. Begins scaffolding out meaningful objects for return

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// TODO: Store libraries and coordinates in a database
+// TODO: Check if the new library exists in the database
+// TODO: If it does not, fetch the coordinates and store them in the database
+
 const { config } = require('dotenv');
 const { Client } = require('@googlemaps/google-maps-services-js');
 const client = new Client({});
@@ -5,57 +9,85 @@ const fs = require('fs');
 const { parse } = require('csv-parse');
 
 config();
+const lendingLibraries = [];
 
-fs.createReadStream('./borrowing-sep-csv.csv')
-  .pipe(parse({ delimiter: ',', from_line: 2 }))
-  .on('data', (row) => {
-    console.log(row);
-  });
-
-const sampleInfo = [
-  'ALACHUA CNTY LIBR DIST',
-  'ALAMANCE CNTY PUB LIBRS',
-  'AMARILLO PUB LIBR',
-  'ANACORTES PUB LIBR',
-  'ANCHORAGE PUB LIBR'
-];
-
-const writeStream = fs.createWriteStream('locations.json', {
-  flags: 'a'
-});
-
-writeStream.write('[');
-
-sampleInfo.forEach((library, index) => {
-  client
-    .findPlaceFromText({
-      params: {
-        input: library,
-        inputtype: 'textquery',
-        fields: ['name', 'geometry'],
-        key: process.env.GOOGLE_API_KEY
-      },
-      timeout: 1000
-    })
-    .then(({ data }) => {
-      const locationData = {
-        name: data.candidates[0].name,
-        latitude: data.candidates[0].geometry.location.lat,
-        longitude: data.candidates[0].geometry.location.lng,
-        institutionSymbol: null,
-        date: {
-          year: 2022,
-          month: 7
-        }
-      };
-      const fileContent = JSON.stringify(locationData);
-      if (index === sampleInfo.length - 1) {
-        writeStream.write(fileContent + ']');
-      } else {
-        writeStream.write(fileContent + ',');
+const extractDataFromCSV = () => {
+  const convertedJSON = fs.createWriteStream('lendingLibraries.json');
+  fs.createReadStream('./borrowing-sep-csv.csv')
+    .pipe(parse({ delimiter: ',', from_line: 2 }))
+    .on('data', (row) => {
+      // If library actually lent books, add to array
+      if (row[7] > 0) {
+        const locationInfo = {
+          name: row[0],
+          institutionSymbol: row[1],
+          institutionState: row[2],
+          requestsFilled: row[7]
+        };
+        lendingLibraries.push(locationInfo);
       }
     })
-    .catch((e) => {
-      console.log(e);
+    .on('end', () => {
+      convertedJSON.write(JSON.stringify(lendingLibraries));
+      fetchCoordinates();
     });
-});
+};
+
+const fetchCoordinates = () => {
+  // const sampleInfo = [
+  //   lendingLibraries[0]
+  // 'ALACHUA CNTY LIBR DIST'
+  // 'ALAMANCE CNTY PUB LIBRS',
+  // 'AMARILLO PUB LIBR',
+  // 'ANACORTES PUB LIBR',
+  // 'ANCHORAGE PUB LIBR'
+  // ];
+
+  const sampleInfo = lendingLibraries.slice(0, 15);
+  const writeStream = fs.createWriteStream('locations.json', {
+    flags: 'a'
+  });
+
+  writeStream.write('[');
+
+  sampleInfo.forEach((library, index) => {
+    client
+      .findPlaceFromText({
+        params: {
+          input: library.name,
+          inputtype: 'textquery',
+          fields: ['name', 'geometry'],
+          key: process.env.GOOGLE_API_KEY
+        },
+        timeout: 1000
+      })
+      .then(({ data }) => {
+        const locationData = {
+          googleMapsName: data.candidates[0].name,
+          name: library.name,
+          latitude: data.candidates[0].geometry.location.lat,
+          longitude: data.candidates[0].geometry.location.lng,
+          institutionSymbol: library.institutionSymbol,
+          institutionState: library.institutionState,
+          date: {
+            year: 2022,
+            month: 9
+          }
+        };
+        libraryDetails.push(locationData);
+        const fileContent = JSON.stringify(locationData);
+        // TODO: This is not actually working anymore!
+        // If the library is the last one in the array, close the array, otherwise add a comma
+        if (index === sampleInfo.length - 1) {
+          writeStream.write(fileContent + ']');
+        } else {
+          writeStream.write(fileContent + ',');
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+};
+
+extractDataFromCSV();
