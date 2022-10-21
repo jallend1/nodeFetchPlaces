49f033f480ac2bcd92a9e8aa34bcfd26ad315e1a
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-10-20 17:15:34 -0700

    Alters folder structure and adds clarifying comments

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+// TODO: Adjust CSV handling so it does not require any editing
 // TODO: Store libraries and coordinates in a database
 // TODO: Check if the new library exists in the database
 // TODO: If it does not, fetch the coordinates and add it to the database
-// TODO: Incorporate state as a parameter for the fetch request
 
 const { config } = require('dotenv');
 const { Client } = require('@googlemaps/google-maps-services-js');
 const client = new Client({});
 const fs = require('fs');
 const { parse } = require('csv-parse');
-const states = require('./stateBias.json');
-
-console.log(states.findIndex((state) => state.name === 'Wyoming'));
-config();
+const states = require('./assets/stateBias.json');
 const lendingLibraries = [];
+
+config();
 
 const extractDataFromCSV = () => {
   const convertedJSON = fs.createWriteStream('lendingLibraries.json');
@@ -36,8 +35,20 @@ const extractDataFromCSV = () => {
     });
 };
 
+// Sets the location bias to the state of the library
+const determineLocationBias = (library) => {
+  const targetState = states.find(
+    (state) => state.abbreviation === library.institutionState
+  );
+  if (targetState) {
+    return [targetState['latitude'], targetState['longitude']];
+  } else {
+    return null;
+  }
+};
+
 const fetchCoordinates = () => {
-  // Hardcoded for testing purposes
+  // Hardcoded to only two libraries for testing purposes
   const sampleInfo = lendingLibraries.slice(0, 2);
   const writeStream = fs.createWriteStream('locations.json', {
     flags: 'a'
@@ -45,13 +56,7 @@ const fetchCoordinates = () => {
 
   const libraryDetails = [];
   sampleInfo.forEach((library, index) => {
-    let stateBias;
-    const targetState = states.find(
-      (state) => state.abbreviation === library.institutionState
-    );
-    targetState
-      ? (stateBias = [targetState['latitude'], targetState['longitude']])
-      : null;
+    const stateBias = determineLocationBias(library);
     client
       .findPlaceFromText({
         params: {
@@ -59,7 +64,7 @@ const fetchCoordinates = () => {
           inputtype: 'textquery',
           fields: ['name', 'geometry'],
           key: process.env.GOOGLE_API_KEY,
-          locationbias: stateBias ? `point:${targetState}` : null
+          locationbias: stateBias
         },
         timeout: 1000
       })
